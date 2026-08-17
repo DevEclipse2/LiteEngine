0fe69dcd96fb65cbe21c308a7bb5065ed757d8aa
DevEclipse2 <151531603+DevEclipse2@users.noreply.github.com>
2026-08-17 23:18:23 +0800

    last raging before the light

--- LiteNgine/core/vulkan/EngineClasses/systems/names.cpp
+++ LiteNgine/core/vulkan/EngineClasses/systems/names.cpp
@@ -1,56 +1,56 @@
-#include "names.h"
-#include <algorithm>
-#include "../Lt_Console.h"
-namespace lte {
-	void names::RegisterName(std::string& Name, uint32_t SlateId, char useBits)
-	{
-		auto it = slateToIndexMap.find(SlateId);
-
-		if (it != slateToIndexMap.end())
-		{
-			// ID is already registered, just overwrite the data
-			Con::Log("slateid already in use, modifying present values", TAG_ENGINE);
-
-			uint32_t index = it->second; // Get the array index from the map
-
-			nameComponents[index].name = Name;
-			nameComponents[index].usebits = useBits | in_use;
-			return;
-		}
-		slateName slate;
-		slate.name = Name;
-		slate.usebits = useBits | in_use;
-		if (!freeSpaces.empty())
-		{
-			uint32_t freeIndex = freeSpaces.back();
-			freeSpaces.pop_back();
-			nameComponents[freeIndex] = slate;
-			AssignedSlates[freeIndex] = SlateId;
-			slateToIndexMap[SlateId] = freeIndex;
-		}
-		else
-		{
-			nameComponents.push_back(slate);
-			AssignedSlates.push_back(SlateId);
-			slateToIndexMap[SlateId] = freeIndex;
-		}
-	}
-	void names::RemoveName(uint32_t SlateId)
-	{
-		if (SlateId == static_cast<uint32_t>(-1))
-		{
-			Con::LogError("invalid slateid (-1)", MED_SEVERITY, TAG_ENGINE);
-		}
-		auto it = slateToIndexMap.find(SlateId);
-
-		if (it != slateToIndexMap.end())
-		{
-			Con::Log("found, tagging for overwrite", TAG_ENGINE);
-			uint32_t index = it->second;
-			freeSpaces.push_back(index);
-			AssignedSlates[index] = static_cast<uint32_t>(-1);
-			nameComponents[index].usebits &= ~in_use;
-			slateToIndexMap.erase(it);
-		}
-	}
-}
+//#include "names.h"
+//#include <algorithm>
+//#include "../Lt_Console.h"
+//namespace lte {
+//	void names::RegisterName(std::string& Name, uint32_t SlateId, char useBits)
+//	{
+//		auto it = slateToIndexMap.find(SlateId);
+//
+//		if (it != slateToIndexMap.end())
+//		{
+//			// ID is already registered, just overwrite the data
+//			Con::Log("slateid already in use, modifying present values", TAG_ENGINE);
+//
+//			uint32_t index = it->second; // Get the array index from the map
+//
+//			nameComponents[index].name = Name;
+//			nameComponents[index].usebits = useBits | in_use;
+//			return;
+//		}
+//		slateName slate;
+//		slate.name = Name;
+//		slate.usebits = useBits | in_use;
+//		if (!freeSpaces.empty())
+//		{
+//			uint32_t freeIndex = freeSpaces.back();
+//			freeSpaces.pop_back();
+//			nameComponents[freeIndex] = slate;
+//			AssignedSlates[freeIndex] = SlateId;
+//			slateToIndexMap[SlateId] = freeIndex;
+//		}
+//		else
+//		{
+//			nameComponents.push_back(slate);
+//			AssignedSlates.push_back(SlateId);
+//			slateToIndexMap[SlateId] = freeIndex;
+//		}
+//	}
+//	void names::RemoveName(uint32_t SlateId)
+//	{
+//		if (SlateId == static_cast<uint32_t>(-1))
+//		{
+//			Con::LogError("invalid slateid (-1)", MED_SEVERITY, TAG_ENGINE);
+//		}
+//		auto it = slateToIndexMap.find(SlateId);
+//
+//		if (it != slateToIndexMap.end())
+//		{
+//			Con::Log("found, tagging for overwrite", TAG_ENGINE);
+//			uint32_t index = it->second;
+//			freeSpaces.push_back(index);
+//			AssignedSlates[index] = static_cast<uint32_t>(-1);
+//			nameComponents[index].usebits &= ~in_use;
+//			slateToIndexMap.erase(it);
+//		}
+//	}
+//}
--- LiteNgine/core/vulkan/EngineClasses/systems/names.h
+++ LiteNgine/core/vulkan/EngineClasses/systems/names.h
@@ -1,28 +1,30 @@
-#pragma once
-#include <string>
-#include <vector>
-#define in_use 1
-#include <WinNls.h>
-#include <stringapiset.h>
-namespace lte
-{
-	struct slateName
-	{
-		std::string name;
-		char usebits;
-	};
-	class names
-	{
-	public:
-		inline static std::vector<slateName> nameComponents;
-		inline static std::vector<uint32_t> AssignedSlates;
-		inline static std::vector<uint32_t> freeSpaces;
-
-		inline static std::unordered_map<uint32_t, uint32_t> slateToIndexMap;
-
-		static void RegisterName(const std::string& Name, uint32_t SlateId, char useBits);
-		static void RemoveName(uint32_t SlateId);
-	};
-}
-
-
+//#pragma once
+//#include <string>
+//#include <vector>
+//#include <windows.h> 
+//#define in_use 1
+//#include <WinNls.h>
+//#include <stringapiset.h>
+//#include <unordered_map>
+//namespace lte
+//{
+//	struct slateName
+//	{
+//		std::string name;
+//		char usebits;
+//	};
+//	class names
+//	{
+//	public:
+//		inline static std::vector<slateName> nameComponents;
+//		inline static std::vector<uint32_t> AssignedSlates;
+//		inline static std::vector<uint32_t> freeSpaces;
+//
+//		inline static std::unordered_map<uint32_t, uint32_t> slateToIndexMap;
+//
+//		static void RegisterName(const std::string& Name, uint32_t SlateId, char useBits);
+//		static void RemoveName(uint32_t SlateId);
+//	};
+//}
+//
+//
